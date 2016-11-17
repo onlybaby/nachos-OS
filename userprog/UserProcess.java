e57458a2b0ccf828a890099a2eb5361b38c2f553
kathy31629 <kathy31629@gmail.com>
2016-11-16 18:18:34 -0800

    fix read and write

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -38,6 +38,7 @@ public class UserProcess {
         for (int i = 2; i < MAXFILE; i++){
             fileTable[i] = null;
         }
+        
     }
     
     /**
@@ -432,7 +433,7 @@ public class UserProcess {
         OpenFile temp = fileTable[fd];	//to find the correct page
         if (temp == null) return -1;	//if page not find return -1
         
-        while (length < 0){				//every time read 1024 bytes or length bytes
+        while (length > 0){				//every time read 1024 bytes or length bytes
             int length_or_1024 = 0;
             if (length > maxBufferSize ) {
                 length_or_1024 = maxBufferSize; //transfer 1024 bytes if length is greater than 1024
@@ -465,7 +466,7 @@ public class UserProcess {
         OpenFile temp = fileTable[fd];	//to find the correct page
         if (temp == null) return -1;	//if page not find return -1
         
-        while (length < 0){				//every time read 1024 bytes or length bytes
+        while (length > 0){				//every time read 1024 bytes or length bytes
             int length_or_1024 = 0;
             if (length > maxBufferSize ) {
                 length_or_1024 = maxBufferSize; //transfer 1024 bytes if length is greater than 1024
@@ -645,6 +646,6 @@ public class UserProcess {
     
     private static final char dbgProcess = 'a';
     
-    protected OpenFile[] fileTable = new OpenFile[16];
+    protected OpenFile[] fileTable;
     protected static final int MAXFILE = 16;
 }

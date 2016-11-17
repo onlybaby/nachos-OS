72e7be51127ca71a3fc3663f78012833791dbf13
kathy31629 <kathy31629@gmail.com>
2016-11-16 18:18:10 -0800

    Revert "fix part1"
    This reverts commit 723dafbb0f27ca07bf47f6654171f00dffe04a1f.

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -35,6 +35,9 @@ public class UserProcess {
         fileTable[0] = UserKernel.console.openForReading();
         fileTable[1] = UserKernel.console.openForWriting();
         
+        for (int i = 2; i < MAXFILE; i++){
+            fileTable[i] = null;
+        }
     }
     
     /**
@@ -429,7 +432,7 @@ public class UserProcess {
         OpenFile temp = fileTable[fd];	//to find the correct page
         if (temp == null) return -1;	//if page not find return -1
         
-        while (length > 0){				//every time read 1024 bytes or length bytes
+        while (length < 0){				//every time read 1024 bytes or length bytes
             int length_or_1024 = 0;
             if (length > maxBufferSize ) {
                 length_or_1024 = maxBufferSize; //transfer 1024 bytes if length is greater than 1024
@@ -462,7 +465,7 @@ public class UserProcess {
         OpenFile temp = fileTable[fd];	//to find the correct page
         if (temp == null) return -1;	//if page not find return -1
         
-        while (length > 0){				//every time read 1024 bytes or length bytes
+        while (length < 0){				//every time read 1024 bytes or length bytes
             int length_or_1024 = 0;
             if (length > maxBufferSize ) {
                 length_or_1024 = maxBufferSize; //transfer 1024 bytes if length is greater than 1024
@@ -642,6 +645,6 @@ public class UserProcess {
     
     private static final char dbgProcess = 'a';
     
-    protected OpenFile[] fileTable;
+    protected OpenFile[] fileTable = new OpenFile[16];
     protected static final int MAXFILE = 16;
 }

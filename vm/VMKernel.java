585d33a99d17cc21994f01cbe322fe7b1ad380b6
kathy31629 <kathy31629@gmail.com>
2016-12-02 15:18:56 -0800

    part 1 fixed

--- vm/VMKernel.java
+++ vm/VMKernel.java
@@ -1,5 +1,7 @@
 package nachos.vm;
 
+import java.util.LinkedList;
+
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
@@ -9,43 +11,90 @@ import nachos.vm.*;
  * A kernel that can support multiple demand-paging user processes.
  */
 public class VMKernel extends UserKernel {
-	/**
-	 * Allocate a new VM kernel.
-	 */
-	public VMKernel() {
-		super();
-	}
-
-	/**
-	 * Initialize this kernel.
-	 */
-	public void initialize(String[] args) {
-		super.initialize(args);
-	}
-
-	/**
-	 * Test this kernel.
-	 */
-	public void selfTest() {
-		super.selfTest();
-	}
-
-	/**
-	 * Start running user programs.
-	 */
-	public void run() {
-		super.run();
-	}
-
-	/**
-	 * Terminate this kernel. Never returns.
-	 */
-	public void terminate() {
-		super.terminate();
-	}
-
-	// dummy variables to make javac smarter
-	private static VMProcess dummy1 = null;
-
-	private static final char dbgVM = 'v';
+    /**
+     * Allocate a new VM kernel.
+     */
+    public VMKernel() {
+        super();
+    }
+    
+    /**
+     * Initialize this kernel.
+     */
+    public void initialize(String[] args) {
+        super.initialize(args);
+        //VMFreePage = UserKernel.freePage;
+        
+        OpenFile file = ThreadedKernel.fileSystem.open(swapFileName, true);
+        victim = 0;
+        invertedPT = new invertedData[Machine.processor().getNumPhysPages()];
+    }
+    
+    /**
+     * Test this kernel.
+     */
+    public void selfTest() {
+        super.selfTest();
+    }
+    
+    /**
+     * Start running user programs.
+     */
+    public void run() {
+        super.run();
+    }
+    
+    //update the inverted table
+    public static boolean swapIn(){
+        return true;
+    }
+    public static int pageAllocation(){
+        if(freePage.size()!= 0){
+            return freePage.removeFirst();
+        }
+        //Replacement algorithm
+        while(invertedPT[victim].entry.used == true){
+            invertedPT[victim].entry.used = false;
+            victim = (victim + 1) % invertedPT.length;
+        }
+        
+        int toEvict = victim;
+        victim = (victim + 1) % invertedPT.length;
+        return toEvict;
+    }
+    
+    public static class invertedData {
+        VMProcess process;
+        TranslationEntry entry;
+        boolean pinned;
+        public invertedData(VMProcess process, TranslationEntry entry, boolean pinned){
+            this.process = process;
+            this.entry = entry;
+            this.pinned = pinned;
+        }
+        public TranslationEntry getEntry(){
+            return this.entry;
+        }
+        
+    }
+    /**
+     * Terminate this kernel. Never returns.
+     */
+    public void terminate() {
+        //swapFile.close();
+        //ThreadedKernel.fileSystem.remove(swapFile.getName());
+        super.terminate();
+    }
+    
+    
+    // dummy variables to make javac smarter
+    private static VMProcess dummy1 = null;
+    
+    private static final char dbgVM = 'v';
+    //public static LinkedList <Integer> VMFreePage = new LinkedList <Integer>();
+    private static LinkedList<Integer> freeSwapPages;
+    public static OpenFile swapFile;
+    public static String swapFileName = ".TEM";
+    public static invertedData[] invertedPT;
+    public static int victim; 
 }
--- vm/VMProcess.java
+++ vm/VMProcess.java
@@ -9,39 +9,38 @@ import nachos.vm.*;
  * A <tt>UserProcess</tt> that supports demand-paging.
  */
 public class VMProcess extends UserProcess {
-	/**
-	 * Allocate a new process.
-	 */
-	public VMProcess() {
-		super();
-		this.lastVPN = 0;
-	}
-
-	/**
-	 * Save the state of this process in preparation for a context switch.
-	 * Called by <tt>UThread.saveState()</tt>.
-	 */
-	public void saveState() {
-		super.saveState();
-	}
-
-	/**
-	 * Restore the state of this process after a context switch. Called by
-	 * <tt>UThread.restoreState()</tt>.
-	 */
-	public void restoreState() {
-		super.restoreState();
-	}
-
-	/**
-	 * Initializes page tables for this process so that the executable can be
-	 * demand-paged.
-	 * 
-	 * @return <tt>true</tt> if successful.
-	 */
-	protected boolean loadSections() {
-		UserKernel.memLock.acquire(); //require a lock when we do memory allocation
-
+    /**
+     * Allocate a new process.
+     */
+    public VMProcess() {
+        super();
+    }
+    
+    /**
+     * Save the state of this process in preparation for a context switch.
+     * Called by <tt>UThread.saveState()</tt>.
+     */
+    public void saveState() {
+        super.saveState();
+    }
+    
+    /**
+     * Restore the state of this process after a context switch. Called by
+     * <tt>UThread.restoreState()</tt>.
+     */
+    public void restoreState() {
+        super.restoreState();
+    }
+    
+    /**
+     * Initializes page tables for this process so that the executable can be
+     * demand-paged.
+     *
+     * @return <tt>true</tt> if successful.
+     */
+    protected boolean loadSections() {
+        UserKernel.memLock.acquire(); //require a lock when we do memory allocation
+        
         if (numPages > UserKernel.freePage.size()) {
             UserKernel.memLock.release(); //release the lock and return false if there is not enough physical memory to allocate
             coff.close();
@@ -77,21 +76,19 @@ public class VMProcess extends UserProcess {
             }
         }
         //System.out.println("here~~~~~~~~~~~~~~");
-
+        
         return true;
-	}
-	
-	public int readVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+    }
+    
+    public int readVirtualMemory(int vaddr, byte[] data, int offset, int length) {
         Lib.assertTrue(offset >= 0 && length >= 0
                        && offset + length <= data.length);
         
         byte[] memory = Machine.processor().getMemory();
         
         int amount = 0;
-        int checkVPN = Processor.pageFromAddress(vaddr);
-        if(pageTable[checkVPN].valid == false){
-            	handlePageFault(checkVPN);
-            } 
+        //int checkVPN = Processor.pageFromAddress(vaddr);
+        
         while (length >0){
             //get the vpn from virtual address
             int vpn = Processor.pageFromAddress(vaddr);
@@ -99,6 +96,9 @@ public class VMProcess extends UserProcess {
             //then it's not valid
             if(vpn<0||vpn>=pageTable.length)
                 break;
+            if(pageTable[vpn].valid == false){
+                handlePageFault(vpn);
+            }
             //pageTable[vpn].used = true;
             //get the offset from virtual address
             int offSet = Processor.offsetFromAddress(vaddr);
@@ -127,19 +127,17 @@ public class VMProcess extends UserProcess {
         
         return amount;
     }
-	
- 	public int writeVirtualMemory(int vaddr, byte[] data, int offset, int length) {
-	//System.out.println("here~~~~~~~~~~~~~~");
+    
+    public int writeVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+        //System.out.println("here~~~~~~~~~~~~~~");
         Lib.assertTrue(offset >= 0 && length >= 0
                        && offset + length <= data.length);
         
         byte[] memory = Machine.processor().getMemory();
         
         int amount = 0;
-        int checkVPN = Processor.pageFromAddress(vaddr);
-        if(pageTable[checkVPN].valid == false){
-            	handlePageFault(checkVPN);
-            } 
+        //int checkVPN = Processor.pageFromAddress(vaddr);
+        
         while (length >0){
             //get the vpn from virtual address
             int vpn = Processor.pageFromAddress(vaddr);
@@ -147,6 +145,9 @@ public class VMProcess extends UserProcess {
             //then it's not valid
             if(vpn<0||vpn>=pageTable.length)
                 break;
+            if(pageTable[vpn].valid == false){
+                handlePageFault(vpn);
+            }
             pageTable[vpn].dirty = true;//set the dirty bit to true
             //pageTable[vpn].used = true;
             //get the offset from virtual address
@@ -174,86 +175,74 @@ public class VMProcess extends UserProcess {
         
         return amount;
     }
-	public void handlePageFault(int faultVPN){
-		if(pageTable[faultVPN].dirty == false){
-			for (int s = 0; s < coff.getNumSections(); s++) {
-				CoffSection section = coff.getSection(s);     
-		        Lib.debug(dbgProcess, "\tinitializing " + section.getName()
-		                      + " section (" + section.getLength() + " pages)");       
-		        for (int i = 0; i < section.getLength(); i++) {
-		        	int vpn = section.getFirstVPN() + i;
-		        //	UserKernel.proLock.acquire();
-		        	//System.out.println("faultVPN is: " + faultVPN + " vpn is : " + vpn);
-		            if(faultVPN == vpn){
-		            	if(UserKernel.freePage.size()>= 0){
-		            		int ppn = UserKernel.freePage.removeFirst();
-		            		if(pageTable[faultVPN] == null) return;
-		            		//System.out.println(ppn);
-		            		pageTable[faultVPN].ppn = ppn;
-		            		section.loadPage(i, ppn);
-		            		pageTable[faultVPN].valid = true;
-		            		pageTable[faultVPN].dirty = true;
-		            		//UserKernel.proLock.release();
-		            		return;
-		            	}
-		            }
-		        }
-		     }
-			/*if(coffPin[faultVPN] == 1){
-				int ppn = UserKernel.freePage.removeFirst();
-				int coffSectNum = coffSectionNum[faultVPN];
-				CoffSection section = coff.getSection(coffSectNum);
-				int num = faultVPN - section.getFirstVPN();
-				section.loadPage(num, ppn);
-				pageTable[faultVPN].ppn = ppn;
-			}*/
-			int ppn = UserKernel.freePage.removeFirst();
-			byte[] memory = Machine.processor().getMemory();
-			byte[] buffer = new byte[pageSize];
-			System.arraycopy(buffer, 0, memory, pageTable[faultVPN].ppn*pageSize, pageSize);
-			pageTable[faultVPN].dirty = true;
-			pageTable[faultVPN].valid = true;
-		}
-	}
-
-	/**
-	 * Release any resources allocated by <tt>loadSections()</tt>.
-	 */
-	protected void unloadSections() {
-		super.unloadSections();
-	}
-
-	/**
-	 * Handle a user exception. Called by <tt>UserKernel.exceptionHandler()</tt>
-	 * . The <i>cause</i> argument identifies which exception occurred; see the
-	 * <tt>Processor.exceptionZZZ</tt> constants.
-	 * 
-	 * @param cause the user exception that occurred.
-	 */
-	public void handleException(int cause) {
-		Processor processor = Machine.processor();
-
-		switch (cause) {
-		case Processor.exceptionPageFault: 
-			int vaddress = processor.readRegister(Processor.regBadVAddr);
-			int vpn = Processor.pageFromAddress(vaddress);
-			handlePageFault(vpn);
-			break;
-		default:
-			super.handleException(cause);
-			break;
-		}
-	}
-
-	private static final int pageSize = Processor.pageSize;
-
-	private static final char dbgProcess = 'a';
-	private int[] coffPage;
-
-	private static final char dbgVM = 'v';
-	private static int lastVPN;
-	private int[] coffSectionNum;
-	private int[] coffPin;
-	//private Lock VMPLock;
-	
-}
+    public void handlePageFault(int faultVPN){
+        if(pageTable[faultVPN].dirty == false){
+            for (int s = 0; s < coff.getNumSections(); s++) {
+                CoffSection section = coff.getSection(s);
+                Lib.debug(dbgProcess, "\tinitializing " + section.getName()
+                          + " section (" + section.getLength() + " pages)");
+                for (int i = 0; i < section.getLength(); i++) {
+                    int vpn = section.getFirstVPN() + i;
+                    if(faultVPN == vpn){
+                        if(UserKernel.freePage.size()>= 0){
+                            int ppn = VMKernel.pageAllocation();
+                            VMKernel.invertedPT[ppn] = new VMKernel.invertedData (this, pageTable[faultVPN], false);
+                            if(pageTable[faultVPN] == null) return;
+                            pageTable[faultVPN].ppn = ppn;
+                            section.loadPage(i, ppn);
+                            pageTable[faultVPN].valid = true;
+                            return;
+                        }
+                    }
+                }
+            }
+            
+            int ppn = VMKernel.pageAllocation();
+            VMKernel.invertedPT[ppn] = new VMKernel.invertedData (this, pageTable[faultVPN], false);
+            pageTable[faultVPN].ppn = ppn;
+            byte[] memory = Machine.processor().getMemory();
+            byte[] buffer = new byte[pageSize];
+            System.arraycopy(buffer, 0, memory, pageTable[faultVPN].ppn*pageSize, pageSize);
+            pageTable[faultVPN].valid = true;
+        }
+    }
+    
+    /**
+     * Release any resources allocated by <tt>loadSections()</tt>.
+     */
+    protected void unloadSections() {
+        super.unloadSections();
+    }
+    
+    /**
+     * Handle a user exception. Called by <tt>UserKernel.exceptionHandler()</tt>
+     * . The <i>cause</i> argument identifies which exception occurred; see the
+     * <tt>Processor.exceptionZZZ</tt> constants.
+     *
+     * @param cause the user exception that occurred.
+     */
+    public void handleException(int cause) {
+        Processor processor = Machine.processor();
+        
+        switch (cause) {
+            case Processor.exceptionPageFault:
+                int vaddress = processor.readRegister(Processor.regBadVAddr);
+                int vpn = Processor.pageFromAddress(vaddress);
+                handlePageFault(vpn);
+                break;
+            default:
+                super.handleException(cause);
+                break;
+        }
+    }
+    
+    private static final int pageSize = Processor.pageSize;
+    
+    private static final char dbgProcess = 'a';
+    private int[] coffPage;
+    
+    private static final char dbgVM = 'v';
+    private static int lastVPN;
+    private int[] coffSectionNum;
+    private int[] coffPin;
+    //private Lock VMPLock;
